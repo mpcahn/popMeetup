c8a122465c06b018d5cc51bc53f9806550d9d4c7
mpcahn <mpcahn@gmail.com>
2018-09-02 12:46:01 -0600

    reloads results on button click, clicks button on return keyUp

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
             return string + this;
     };
 
+    $(".zip").keyup(function (e) {
+        if (e.keyCode === 13) {
+            $(".btn").click()
+        }
+    })
+
     $('.btn').on('click', function () {
         var zip = $('.zip').val()
         var day = $('.date').val()
@@ -20,34 +26,34 @@ document.addEventListener("DOMContentLoaded", function (event) {
         var link = 'https://galvanize-cors.herokuapp.com/https://api.meetup.com/2/open_events?&key=673e4c22272f4632b4c671069d3752&photo-host=public&zip=&page=200'
         link = link.insert(132, zip)
 
-        
-            $.get(link)
-                .then(function (data) {
-                    dataLength = data.results.length
-                    for (var i = 0; i < dataLength; i++) {
-                        data.results[i].group.name
-                        group_name = data.results[i].group.name
-                        num_rsvps = data.results[i].yes_rsvp_count
-                        url = data.results[i].event_url
-                        console.log(data.results)
-                        
-                        obj = {
-                            'name': group_name,
-                            'link': url,
-                            'rsvps': num_rsvps
-                        }
-                        events.push(obj)
+        $(".list-group").empty()
+
+        $.get(link)
+            .then(function (data) {
+                dataLength = data.results.length
+                for (var i = 0; i < dataLength; i++) {
+                    data.results[i].group.name
+                    group_name = data.results[i].group.name
+                    num_rsvps = data.results[i].yes_rsvp_count
+                    url = data.results[i].event_url
+
+                    obj = {
+                        'name': group_name,
+                        'link': url,
+                        'rsvps': num_rsvps
                     }
+                    events.push(obj)
+                }
 
-                    events.sort(function (a, b) {
-                        return b.rsvps - a.rsvps
-                    })
+                events.sort(function (a, b) {
+                    return b.rsvps - a.rsvps
+                })
 
 
-                    for (var i = 0; i < events.length; i++) {
-                        var link = '<li  class="list-group-item">' + '<a href="' + events[i].link + '">' + events[i].rsvps + ' rsvp\'d from: ' + events[i].name + '</a>' + "</li>"
-                        $('.list-group').append(link)
-                    }
-                });
-        })
+                for (var i = 0; i < events.length; i++) {
+                    var link = '<li  class="list-group-item">' + '<a href="' + events[i].link + '">' + events[i].rsvps + ' rsvp\'d from: ' + events[i].name + '</a>' + "</li>"
+                    $('.list-group').append(link)
+                }
+            });
     })
+})
