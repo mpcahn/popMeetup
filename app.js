338d39174d293418d85d8902a20b007eee76a7f5
mpcahn <mpcahn@gmail.com>
2018-09-02 12:22:59 -0600

    update main.css and index.html

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
 //api key 673e4c22272f4632b4c671069d3752
 
-events = []
-eventsDOM = []
 
-String.prototype.insert = function (index, string) {
-    if (index > 0)
-      return this.substring(0, index) + string + this.substring(index, this.length);
-    else
-      return string + this;
-  };
+document.addEventListener("DOMContentLoaded", function (event) {
 
-$(function () {
-    
-    $('.btn').on('click', function () {  
+    events = []
+    eventsDOM = []
 
-        var text = $('.zip').val();
+    String.prototype.insert = function (index, string) {
+        if (index > 0)
+            return this.substring(0, index) + string + this.substring(index, this.length);
+        else
+            return string + this;
+    };
+
+    $('.btn').on('click', function () {
+
+        var zip = $('.zip').val();
+        var day = $('.date').val();
+        console.log(day)
         var link = 'https://galvanize-cors.herokuapp.com/https://api.meetup.com/2/open_events?&key=673e4c22272f4632b4c671069d3752&photo-host=public&zip=&page=200';
-        link = link.insert(132, text);
+        link = link.insert(132, zip);
 
-        $(document).ready(function(){
+
+
+        $(document).ready(function () {
             $.get(link)
-            .then(function(data){
-                dataLength = data.results.length
-                for (var i = 0; i < dataLength; i++) {
-                    data.results[i].group.name
-                    group_name = data.results[i].group.name;
-                    num_rsvps = data.results[i].yes_rsvp_count;
-                    url = data.results[i].event_url;
-                    obj = {
-                        'name': group_name,
-                        'link': url,
-                        'rsvps': num_rsvps
+                .then(function (data) {
+                    dataLength = data.results.length
+                    for (var i = 0; i < dataLength; i++) {
+                        data.results[i].group.name
+                        group_name = data.results[i].group.name;
+                        num_rsvps = data.results[i].yes_rsvp_count;
+                        url = data.results[i].event_url;
+                        console.log(data.results)
+                        
+                        obj = {
+                            'name': group_name,
+                            'link': url,
+                            'rsvps': num_rsvps
+                        }
+                        events.push(obj);
                     }
-                    events.push(obj);                    
-                }     
-                  
-                events.sort(function(a, b){return b.rsvps - a.rsvps}); 
+
+                    events.sort(function (a, b) {
+                        return b.rsvps - a.rsvps
+                    });
 
 
-                for (var i = 0; i < events.length; i++) {
-                    var link = '<li  class="list-group-item">' + '<a href="'+ events[i].link + '">'+ events[i].rsvps + ' rsvp\'d from: ' + events[i].name + '</a>' + "</li>";
-                    $('.list-group').append(link);
-                };
-            });
-        });        
+                    for (var i = 0; i < events.length; i++) {
+                        var link = '<li  class="list-group-item">' + '<a href="' + events[i].link + '">' + events[i].rsvps + ' rsvp\'d from: ' + events[i].name + '</a>' + "</li>";
+                        $('.list-group').append(link);
+                    };
+                });
+        });
 
     });
 });
-
-
-
-
-
-
